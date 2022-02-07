ec501e99e81925c29f908526c9629eebe27da964
Michael Rico <rico.michael@gmail.com>
2022-02-07 13:00:47 -0800

    Add alt version of avl solution

--- specs/avl/avl.solution.test.js
+++ specs/avl/avl.solution.test.js
@@ -22,165 +22,153 @@
 */
 
 class Tree {
-  constructor() {
-    this.root = null;
+  static create(nums) {
+    const tree = new Tree()
+    nums.forEach((x) => tree.add(x))
+    return tree
+  }
+  toObject() {
+    return this.root
+  }
+  constructor(value) {
+    this.root = value && new Node(value)
   }
   add(value) {
     if (!this.root) {
-      this.root = new Node(value);
+      this.root = new Node(value)
     } else {
-      this.root.add(value);
+      this.root.add(value)
     }
-  }
-  toJSON() {
-    return JSON.stringify(this.root.serialize(), null, 4);
-  }
-  toObject() {
-    return this.root.serialize();
   }
 }
 
 class Node {
-  constructor(value = null, left = null, right = null) {
-    this.left = left;
-    this.right = right;
-    this.value = value;
-    this.height = 1;
+  constructor(value, parent) {
+    this.value = value
+    this.parent = parent
+    this.right = null
+    this.left = null
+    this.height = 1
   }
   add(value) {
-    if (value < this.value) {
-      // go left
-
-      if (this.left) {
-        this.left.add(value);
+    if (value > this.value) {
+      if (!this.right) {
+        this.right = new Node(value, this)
       } else {
-        this.left = new Node(value);
-      }
-      if (!this.right || this.right.height < this.left.height) {
-        this.height = this.left.height + 1;
-      }
-    } else {
-      // go right
-
-      if (this.right) {
-        this.right.add(value);
-      } else {
-        this.right = new Node(value);
+        this.right.add(value)
       }
       if (!this.left || this.right.height > this.left.height) {
-        this.height = this.right.height + 1;
+        this.height = this.right.height + 1
+      }
+    } else {
+      if (!this.left) {
+        this.left = new Node(value, this)
+      } else {
+        this.left.add(value)
+      }
+      if (!this.right || this.left.height > this.right.height) {
+        this.height = this.left.height + 1
       }
     }
-    this.balance();
+    this.balance()
   }
   balance() {
-    const rightHeight = this.right ? this.right.height : 0;
-    const leftHeight = this.left ? this.left.height : 0;
-
+    const rightHeight = this.right ? this.right.height : 0
+    const leftHeight = this.left ? this.left.height : 0
     if (leftHeight > rightHeight + 1) {
-      const leftRightHeight = this.left.right ? this.left.right.height : 0;
-      const leftLeftHeight = this.left.left ? this.left.left.height : 0;
-
+      const leftRightHeight = this.left.right ? this.left.right.height : 0
+      const leftLeftHeight = this.left.left ? this.left.left.height : 0
       if (leftRightHeight > leftLeftHeight) {
-        this.left.rotateRR();
+        this.left.rotateLeft()
       }
-
-      this.rotateLL();
+      this.rotateRight()
     } else if (rightHeight > leftHeight + 1) {
-      const rightRightHeight = this.right.right ? this.right.right.height : 0;
-      const rightLeftHeight = this.right.left ? this.right.left.height : 0;
-
+      const rightLeftHeight = this.right.left ? this.right.left.height : 0
+      const rightRightHeight = this.right.right ? this.right.right.height : 0
       if (rightLeftHeight > rightRightHeight) {
-        this.right.rotateLL();
+        this.right.rotateRight()
       }
-
-      this.rotateRR();
+      this.rotateLeft()
     }
   }
-  rotateRR() {
-    const valueBefore = this.value;
-    const leftBefore = this.left;
-    this.value = this.right.value;
-    this.left = this.right;
-    this.right = this.right.right;
-    this.left.right = this.left.left;
-    this.left.left = leftBefore;
-    this.left.value = valueBefore;
-    this.left.updateInNewLocation();
-    this.updateInNewLocation();
+  rotateRight() {
+    const valueBefore = this.value
+    const rightBefore = this.right
+    this.value = this.left.value
+    this.right = this.left
+    this.left = this.left.left
+    this.right.left = this.right.right
+    this.right.right = rightBefore
+    this.right.value = valueBefore
+    this.right.updateHeight()
+    this.updateHeight()
   }
-  rotateLL() {
-    const valueBefore = this.value;
-    const rightBefore = this.right;
-    this.value = this.left.value;
-    this.right = this.left;
-    this.left = this.left.left;
-    this.right.left = this.right.right;
-    this.right.right = rightBefore;
-    this.right.value = valueBefore;
-    this.right.updateInNewLocation();
-    this.updateInNewLocation();
+  rotateLeft() {
+    const valueBefore = this.value
+    const leftBefore = this.left
+    this.value = this.right.value
+    this.left = this.right
+    this.right = this.right.right
+    this.left.right = this.left.left
+    this.left.left = leftBefore
+    this.left.value = valueBefore
+    this.left.updateHeight()
+    this.updateHeight()
   }
-  updateInNewLocation() {
+  updateHeight() {
+    // update heights
     if (!this.right && !this.left) {
-      this.height = 1;
+      this.height = 1
     } else if (
       !this.right ||
       (this.left && this.right.height < this.left.height)
     ) {
-      this.height = this.left.height + 1;
+      this.height = this.left.height + 1
     } else {
       //if (!this.left || this.right.height > this.left.height)
-      this.height = this.right.height + 1;
+      this.height = this.right.height + 1
     }
-  }
-  serialize() {
-    const ans = { value: this.value };
-    ans.left = this.left === null ? null : this.left.serialize();
-    ans.right = this.right === null ? null : this.right.serialize();
-    ans.height = this.height;
-    return ans;
   }
 }
 
 // unit tests
 // do not modify the below code
-describe("AVL Tree", function () {
-  it("creates a correct tree", () => {
-    const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
-    const tree = new Tree();
-    nums.map((num) => tree.add(num));
-    const objs = tree.toObject();
+describe('AVL Tree', function () {
+  test('creates a correct tree', () => {
+    const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8]
+    const tree = Tree.create(nums)
+    const objs = tree.toObject()
+    debugger
 
-    expect(objs.value).toEqual(4);
+    expect(objs.value).toEqual(4)
 
-    expect(objs.left.value).toEqual(2);
+    expect(objs.left.value).toEqual(2)
 
-    expect(objs.left.left.value).toEqual(1);
-    expect(objs.left.left.left).toBeNull();
-    expect(objs.left.left.right).toBeNull();
+    expect(objs.left.left.value).toEqual(1)
+    expect(objs.left.left.left).toBeNull()
+    expect(objs.left.left.right).toBeNull()
 
-    expect(objs.left.right.value).toEqual(3);
-    expect(objs.left.right.left).toBeNull();
-    expect(objs.left.right.right).toBeNull();
+    expect(objs.left.right.value).toEqual(3)
+    expect(objs.left.right.left).toBeNull()
+    expect(objs.left.right.right).toBeNull()
 
-    expect(objs.right.value).toEqual(7);
+    expect(objs.right.value).toEqual(7)
 
-    expect(objs.right.left.value).toEqual(6);
-    expect(objs.right.left.right).toBeNull();
+    expect(objs.right.left.value).toEqual(6)
+    expect(objs.right.left.right).toBeNull()
 
-    expect(objs.right.left.left.value).toEqual(5);
-    expect(objs.right.left.left.left).toBeNull();
-    expect(objs.right.left.left.right).toBeNull();
+    expect(objs.right.left.left.value).toEqual(5)
+    expect(objs.right.left.left.left).toBeNull()
+    expect(objs.right.left.left.right).toBeNull()
 
-    expect(objs.right.right.value).toEqual(9);
+    expect(objs.right.right.value).toEqual(9)
 
-    expect(objs.right.right.left.value).toEqual(8);
-    expect(objs.right.right.left.left).toBeNull();
-    expect(objs.right.right.left.right).toBeNull();
+    expect(objs.right.right.left.value).toEqual(8)
+    expect(objs.right.right.left.left).toBeNull()
+    expect(objs.right.right.left.right).toBeNull()
 
-    expect(objs.right.right.right.value).toEqual(10);
-    expect(objs.right.right.right.left).toBeNull();
-    expect(objs.right.right.right.right).toBeNull();
-  });
-});
+    expect(objs.right.right.right.value).toEqual(10)
+    expect(objs.right.right.right.left).toBeNull()
+    expect(objs.right.right.right.right).toBeNull()
+  })
+})
